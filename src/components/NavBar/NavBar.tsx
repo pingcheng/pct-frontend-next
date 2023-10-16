43d22780d993448e5f10f1a8b39724fac0db4800
Ping Cheng <ping.che@hotmail.com>
2023-10-16 15:12:38 +1100

    navbar supports mobile view

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { profile } from "@/data/profile";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./style.module.scss";
+import { useState } from "react";
 
 type MenuItem = {
   label: string;
@@ -27,12 +28,55 @@ const menuItems: MenuItem[] = [
 
 export function NavBar() {
   const pathName = usePathname();
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+
   return (
     <>
       <nav className="bg-gray-700">
         <div className="fixed-container relative flex items-center justify-between h-16">
           {/* Mobile nav bar*/}
-          <></>
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsMobileMenuVisible(!isMobileMenuVisible)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-expanded="false"
+              role="mobile-toggle"
+            >
+              <span className="sr-only">Open main menu</span>
+
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
 
           {/* Desktop nav bar */}
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -66,6 +110,29 @@ export function NavBar() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+
+        {/* Mobile nav bar */}
+        <div
+          className={`sm:hidden ${isMobileMenuVisible ? "block" : "hidden"}`}
+          role="mobile-nav-menu"
+        >
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {menuItems.map((item) => {
+              const isActive = pathName === item.path;
+              return (
+                <Link
+                  key={item.path}
+                  href={item.path}
+                  className={`${styles.navItem} ${
+                    isActive ? styles["active"] : ""
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              );
+            })}
           </div>
         </div>
       </nav>
