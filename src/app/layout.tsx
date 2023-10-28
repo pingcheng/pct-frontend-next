818d7cbcc98da5352e102cd39ac2b69b82b8f8ad
Ping Cheng <ping.che@hotmail.com>
2023-10-28 21:35:01 +1100

    add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
+import Gtag from "@/components/Gtag/Gtag";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Gtag />
         <Analytics />
       </body>
     </html>
---
+++ src/components/Gtag/Gtag.tsx
@@ -0,0 +1,26 @@
+import Script from "next/script";
+
+export default function Gtag() {
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+  if (!gaMeasurementId) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+      />
+      <Script id="google-anlytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${gaMeasurementId}');
+        `}
+      </Script>
+    </>
+  );
+}
