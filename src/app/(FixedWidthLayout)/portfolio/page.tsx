fd3602bc02ba76cf89168d6128392b12cd3bd957
Ping Cheng <ping.che@hotmail.com>
2023-10-28 16:41:15 +1100

    remove portfolio

--- src/app/(FixedWidthLayout)/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from "@/components/Heading";
-
-export default function Portfolio() {
-  return (
-    <div>
-      <Heading text="Portfolio" align="center" />
-    </div>
-  );
-}
