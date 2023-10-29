d269cde40892318b0fda354abded090aa65a3d1d
Ping Cheng <ping.che@hotmail.com>
2023-10-30 00:12:22 +1100

    add portfolio page base

---
+++ src/app/(FixedWidthLayout)/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@/components/Heading";
+
+export default function Portfolio() {
+  return (
+    <div className="py-4">
+      <Heading text="Portfolio" align="center" />
+    </div>
+  );
+}
