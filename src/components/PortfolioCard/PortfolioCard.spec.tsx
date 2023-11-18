4461d9acbf9073c24d817f1b9219c881c8d890ec
Ping Cheng <ping.che@hotmail.com>
2023-11-19 10:14:35 +1100

    add portfolio card snapshot

---
+++ src/components/PortfolioCard/PortfolioCard.spec.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import PortfolioCard from "@/components/PortfolioCard/PortfolioCard";
+import { Portfolio } from "@/models/Portfolio/Portfolio";
+
+describe("test PortfolioCard", () => {
+  const portfolio: Portfolio = new Portfolio("test-portfolio", {
+    name: "Test Portfolio",
+    coverImage: "https://sample.jpg",
+    url: "https://sample.com",
+    shortDescription: "This is test portfolio description",
+    longDescription: "This is the long version of portfolio description",
+    workplace: "Ping Cheng Dev Space",
+    projectRole: "Developer",
+    roleDescription: ["Design", "Development", "Test"],
+    members: ["Ping Cheng"],
+    screenshots: ["https://image1.jpg", "https://image2.jpg"],
+  });
+
+  it("should match the snapshot", () => {
+    const { container } = render(<PortfolioCard portfolio={portfolio} />);
+    expect(container).toMatchSnapshot();
+  });
+});
