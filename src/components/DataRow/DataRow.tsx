1a1f49666d82f75b2e96087852198c963a0ed9b9
Ping Cheng <ping.che@hotmail.com>
2025-06-13 01:03:41 +1000

    update margin

--- src/components/DataRow/DataRow.tsx
+++ src/components/DataRow/DataRow.tsx
@@ -7,11 +7,11 @@ type DataRowProps = {
 
 export function DataRow({ label, children }: DataRowProps) {
   return (
-    <div className="flex flex-col sm:flex-row items-start sm:items-baseline mb-6 pb-4 border-b border-dashed border-gray-200 dark:border-gray-700 last:border-b-0 last:mb-0 last:pb-0">
-      <div className="text-gray-500 dark:text-gray-400 w-full sm:w-1/4 text-left sm:text-right pr-2 font-semibold tracking-wide uppercase text-xs mb-1 sm:mb-0 pt-0 sm:pt-1">
+    <div className="flex flex-col sm:flex-row items-start sm:items-baseline py-3 border-b border-dashed border-gray-200 dark:border-gray-700 last:border-b-0 last:pb-0">
+      <div className="text-gray-500 dark:text-gray-400 w-full sm:w-1/4 text-left sm:text-right pr-2 sm:pr-3 font-semibold tracking-wide uppercase text-xs pt-0 sm:pt-1">
         {label.trim()}
       </div>
-      <div className="flex-1 pl-0 sm:pl-2 w-full">{children}</div>
+      <div className="flex-1 pl-0 sm:pl-3 w-full">{children}</div>
     </div>
   );
 }
